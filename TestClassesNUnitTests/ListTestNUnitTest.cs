b33bab58e573bf216a45d2c2678786a7c443463b
ruggero61 <ruggerosoftwareengineer@gmail.com>
2023-11-18 10:46:58 +0100

    ListTest

---
+++ TestClassesNUnitTests/ListTestNUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClasses;
+
+namespace TestClasses;
+
+[TestFixture]
+public class ListTestNUnitTest
+{
+  [Test]
+  public void Test()
+  {
+    // Arrange
+    ListTest listTest = new ListTest();
+
+    // Act
+    List<int> l = listTest.TestAddRange();
+    //int y = 3;
+
+    // Assert
+    Assert.Pass("Just getting the console output here...");
+    //Assert.AreEqual(y, l.Count);
+  }
+}
